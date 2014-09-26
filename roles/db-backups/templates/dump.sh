d6e298d113c7a560cbfa3db525ad47f5db722730
Thomas Schank <DrTom@schank.ch>
2014-09-26 10:59:20 +0200

    Use relative instead of absolute linkt for latest backup

--- roles/db-backups/templates/dump.sh
+++ roles/db-backups/templates/dump.sh
@@ -3,9 +3,11 @@
 # Ansible managed 
 
 DUMP_DIR="{{madek_root_dir}}/db/media_files/{{rails_env}}/backups"
-DUMP_FILE="${DUMP_DIR}/madek-dump_$(date +%FT%T%Z).pgbin"
-LINK="${DUMP_DIR}/latest.pgbin"
+DUMP_FILE_NAME="madek-dump_$(date +%FT%T%Z).pgbin"
+DUMP_FILE="${DUMP_DIR}/${DUMP_FILE_NAME}"
+LINK_NAME="latest.pgbin"
+LINK="${DUMP_DIR}/${LINK_NAME}"
 mkdir -p ${DUMP_DIR}
 pg_dump -E utf-8 -x -F c -d madek_{{rails_env}} -f ${DUMP_FILE}
-ln -s -f ${DUMP_FILE} ${LINK}
 find ${DUMP_DIR} -name 'madek-dump*pgbin' -type f -mtime +10 -exec rm {} \;
+cd ${DUMP_DIR} && ln -s -f ${DUMP_FILE_NAME} ${LINK_NAME}
