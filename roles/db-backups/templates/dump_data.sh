efceb011d4b546b96ac39b35df5123b07b54153a
Thomas Schank <DrTom@schank.ch>
2015-01-16 09:49:16 +0100

    Add data_dump feature; update backup scripts

--- roles/db-backups/templates/dump_data.sh
+++ roles/db-backups/templates/dump_data.sh
@@ -3,11 +3,11 @@
 # Ansible managed 
 
 DUMP_DIR="{{madek_root_dir}}/db/media_files/{{rails_env}}/backups"
-DUMP_FILE_NAME="madek-dump_$(date +%FT%T%Z).pgbin"
+DUMP_FILE_NAME="madek-data-dump_$(date +%FT%T%Z).pgbin"
 DUMP_FILE="${DUMP_DIR}/${DUMP_FILE_NAME}"
-LINK_NAME="latest.pgbin"
+LINK_NAME="latest-data.pgbin"
 LINK="${DUMP_DIR}/${LINK_NAME}"
 mkdir -p ${DUMP_DIR}
-pg_dump -F c -x -O -f ${DUMP_FILE} madek_{{rails_env}} 
-find ${DUMP_DIR} -name 'madek-dump*pgbin' -type f -mtime +10 -exec rm {} \;
+pg_dump -F c -a -T schema_migrations -x -O -f ${DUMP_FILE} madek_{{rails_env}} 
+find ${DUMP_DIR} -name 'madek-data-dump*pgbin' -type f -mtime +10 -exec rm {} \;
 cd ${DUMP_DIR} && ln -s -f ${DUMP_FILE_NAME} ${LINK_NAME}
